38339ea3d98b3f14173d1c494c660ee03b920fac
hanhui <193691140@qq.com>
2022-03-24 16:38:20 +0800

    update gcn and gat

--- examples/gat/gat_trainer.py
+++ examples/gat/gat_trainer.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-@File    :   gcn_trainer.py
-@Time    :   2021/11/02 22:05:55
+@File    :   gat_trainer.py
+@Time    :   2021/11/11 15:57:45
 @Author  :   hanhui
 """
 
@@ -15,26 +15,14 @@ import time
 import argparse
 import tensorlayerx as tlx
 from gammagl.datasets import Planetoid
-from gammagl.models import GCNModel
+from gammagl.models import GATModel
 from gammagl.utils.config import Config
 from tensorlayerx.model import TrainOneStep, WithLoss
 
-class SemiSpvzLoss(WithLoss):
-    def __init__(self, net, loss_fn):
-        super(SemiSpvzLoss, self).__init__(backbone=net, loss_fn=loss_fn)
-
-    def forward(self, data, label):
-        logits = self._backbone(data['node_feat'], data['edge_index'], data['edge_weight'], data['num_nodes'])
-        train_logits = logits[data['train_mask']]
-        train_label = label[data['train_mask']]
-        loss = self._loss_fn(train_logits, train_label)
-        # # it semms the next line wont take efect
-        # l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in self._backbone.trainable_weights[0]]) * args.l2_coef # only support for tensorflow backend
-        return loss
 
 def evaluate(net, data, node_label, mask, metrics):
     net.set_eval()
-    logits = net(data['node_feat'], data['edge_index'], data['edge_weight'], data['num_nodes'])
+    logits = net(data['node_feat'], data['edge_index'], data['num_nodes'])
     _logits = logits[mask]
     _label = node_label[mask]
     metrics.update(_logits, _label)
@@ -42,34 +30,45 @@ def evaluate(net, data, node_label, mask, metrics):
     metrics.reset()
     return acc
 
+
+class SemiSpvzLoss(WithLoss):
+    def __init__(self, net, loss_fn):
+        super(SemiSpvzLoss, self).__init__(backbone=net, loss_fn=loss_fn)
+
+    def forward(self, data, label):
+        logits = self._backbone(data['node_feat'], data['edge_index'], data['num_nodes'])
+        train_logits = logits[data['train_mask']]
+        train_label = label[data['train_mask']]
+        loss = self._loss_fn(train_logits, train_label)
+        return loss
+
+
+
 def main(args):
     # load cora dataset
-    if str.lower(args.dataset) not in ['cora','pubmed','citeseer']:
+    if str.lower(args.dataset) not in ['cora', 'pubmed', 'citeseer']:
         raise ValueError('Unknown dataset: {}'.format(args.dataset))
     dataset = Planetoid("../", args.dataset)
-    dataset.process() # suggest to execute explicitly so far
+    dataset.process()  # suggest to execute explicitly so far
     graph = dataset[0]
-    if args.self_loop: graph.add_self_loop(n_loops=1) # self-loop trick
-    edge_weight = tlx.ops.convert_to_tensor(GCNModel.calc_gcn_norm(graph.edge_index, graph.num_nodes))
-    node_feat = graph.x
+    graph.add_self_loop(n_loops=1)  # self-loop trick
     edge_index = graph.edge_index
-    node_label = tlx.argmax(graph.y,1)
+    node_feat = graph.x
+    node_label = tlx.argmax(graph.y, 1)
 
-    # configurate
+    # configurate and build model
     cfg = Config(feature_dim=node_feat.shape[1],
                  hidden_dim=args.hidden_dim,
                  num_class=graph.y.shape[1],
-                 keep_rate=args.keep_rate,)
+                 heads=args.heads,
+                 keep_rate=args.keep_rate)
 
-
-    # build model
     best_model_path = r'./best_models/'
     if not os.path.exists(best_model_path): os.makedirs(best_model_path)
-
     loss = tlx.losses.softmax_cross_entropy_with_logits
     optimizer = tlx.optimizers.Adam(args.lr)
     metrics = tlx.metrics.Accuracy()
-    net = GCNModel(cfg, name="GCN")
+    net = GATModel(cfg, name="GAT")
     train_weights = net.trainable_weights
 
     loss_func = SemiSpvzLoss(net, loss)
@@ -78,7 +77,6 @@ def main(args):
     data = {
         "node_feat": node_feat,
         "edge_index": edge_index,
-        "edge_weight": edge_weight,
         "train_mask": graph.train_mask,
         "test_mask": graph.test_mask,
         "val_mask": graph.val_mask,
@@ -91,9 +89,9 @@ def main(args):
         train_loss = train_one_step(data, node_label)
         val_acc = evaluate(net, data, node_label, data['val_mask'], metrics)
 
-        print("Epoch [{:0>3d}] ".format(epoch+1)\
-              + "  train loss: {:.4f}".format(train_loss)\
-              + "  val acc: {:.4f}".format(val_acc))
+        print("Epoch [{:0>3d}]  ".format(epoch + 1)\
+              + "   train loss: {:.4f}".format(train_loss)\
+              + "   val acc: {:.4f}".format(val_acc))
 
         # save best model on evaluation set
         if val_acc > best_val_acc:
@@ -104,15 +102,15 @@ def main(args):
     test_acc = evaluate(net, data, node_label, data['test_mask'], metrics)
     print("Test acc:  {:.4f}".format(test_acc))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # parameters setting
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lr", type=float, default=0.01, help="learnin rate")
+    parser.add_argument("--lr", type=float, default=0.005, help="learnin rate")
     parser.add_argument("--n_epoch", type=int, default=200, help="number of epoch")
-    parser.add_argument("--hidden_dim", type=int, default=16, help="dimention of hidden layers")
-    parser.add_argument("--keep_rate", type=float, default=0.5, help="keep_rate = 1 - drop_rate")
+    parser.add_argument("--hidden_dim", type=int, default=8, help="dimention of hidden layers")
+    parser.add_argument("--keep_rate", type=float, default=0.4, help="keep_rate = 1 - drop_rate")
     parser.add_argument("--l2_coef", type=float, default=5e-4, help="l2 loss coeficient")
+    parser.add_argument("--heads", type=int, default=8, help="number of heads for stablization")
     parser.add_argument('--dataset', type=str, default='cora', help='dataset')
     parser.add_argument("--self_loop", action='store_false', default=True, help="graph self-loop (default=True)")
     args = parser.parse_args()
