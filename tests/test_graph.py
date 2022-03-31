0f6d2663e5c132f64fd2226d0c64043adbb09101
Theheavens <34649403+Theheavens@users.noreply.github.com>
2022-03-31 15:12:24 +0800

    update

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,7 +4,7 @@ from gammagl.data import Graph
 
 def test_data():
 
-    x = tlx.ops.constant([[1, 3, 5], [2, 4, 6]], dtype=tlx.float32)
+    x = tlx.ops.constant([[1, 3], [2, 4], [1, 2]], dtype=tlx.float32)
     edge_index = tlx.ops.constant([[0, 0, 1, 1, 2], [1, 1, 0, 2, 1]])
     data = Graph(x=x, edge_index=edge_index)
 
