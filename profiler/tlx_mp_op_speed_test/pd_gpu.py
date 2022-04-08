ce610906db0ed38748741573f45fe4707521d929
hanhui <193691140@qq.com>
2022-04-08 20:03:30 +0800

    update profiler

--- profiler/tlx_mp_op_speed_test/pd_gpu.py
+++ profiler/tlx_mp_op_speed_test/pd_gpu.py
@@ -8,7 +8,7 @@
 import os
 os.environ['TL_BACKEND'] = 'paddle'
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-import time
+from pyinstrument import Profiler
 import numpy as np
 import tensorlayerx as tlx
 
@@ -20,23 +20,24 @@ dst = edge_index[1,:]
 src = tlx.convert_to_tensor(src, tlx.int64)
 dst = tlx.convert_to_tensor(dst, tlx.int64)
 x = tlx.convert_to_tensor(np.random.randn(num_nodes, 1000), dtype=tlx.float32)
+pf = Profiler()
 
 
-start = time.time()
+pf.start()
 for j in range(1000):
     msg = tlx.gather(x, src)
     tlx.unsorted_segment_sum(msg, dst, num_nodes)
-end = time.time()
-print('paddle unsorted spends {:.5f} s'.format(end-start))
+pf.stop()
+print(pf.output_text(unicode=True, color=True))
 
 
 dst = tlx.convert_to_numpy(dst)
 idx = np.argsort(dst)
 dst = tlx.gather(tlx.convert_to_tensor(dst, dtype=tlx.int64), tlx.convert_to_tensor(idx,dtype=tlx.int64))
 
-start = time.time()
+pf.start()
 for j in range(1000):
     msg = tlx.gather(x, src)
     tlx.segment_sum(msg, dst)
-end = time.time()
-print('paddle sorted spends {:.5f} s'.format(end-start))
+pf.stop()
+print(pf.output_text(unicode=True, color=True))
