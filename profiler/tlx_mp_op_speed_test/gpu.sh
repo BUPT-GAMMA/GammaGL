6327f2045723bc2ca0660a5407f3b67112c7dfc4
hanhui <193691140@qq.com>
2022-04-11 10:18:38 +0800

    update mpops profiler

---
+++ profiler/tlx_mp_op_speed_test/gpu.sh
@@ -0,0 +1,5 @@
+python th_gpu.py
+python tf_gpu.py
+python ms_gpu.py
+python pd_gpu.py
+python pyg_gpu.py
--- profiler/tlx_mp_op_speed_test/th_gpu.py
+++ profiler/tlx_mp_op_speed_test/th_gpu.py
@@ -7,11 +7,14 @@
 
 import os
 os.environ['TL_BACKEND'] = 'torch'
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "8"
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 import torch
 from pyinstrument import Profiler
 import numpy as np
 import tensorlayerx as tlx
+import gammagl.mpops as mpops
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -28,6 +31,34 @@ def unsorted_segment_sum(x, segment_ids, num_segments):
     shape = [num_segments] + list(x.shape[1:])
     tensor = torch.zeros(*shape, dtype=x.dtype,device=device).scatter_add(0, segment_ids, x)
     return tensor
+
+def unsorted_segment_mean(x, segment_ids, num_segments=None):
+    assert x.shape[0] == segment_ids.shape[0], "the length of segment_ids should be equal to data.shape[0]."
+    if num_segments is not None:
+        assert segment_ids.max() < num_segments
+
+    if len(segment_ids.shape) == 1:
+        s = torch.prod(torch.tensor(x.shape[1:])).to(torch.int32).item()
+        segment_ids = segment_ids.repeat_interleave(s).view(segment_ids.shape[0], *x.shape[1:])
+
+    assert x.shape == segment_ids.shape, "data.shape and segment_ids.shape should be equal"
+
+    shape = [num_segments] + list(x.shape[1:])
+    ones_data = torch.ones_like(x, dtype=x.dtype, device=device)
+    tensor = torch.zeros(*shape, dtype=x.dtype,device=device).scatter_add(0, segment_ids, x)
+    tensor_nums = torch.zeros(*shape, dtype=x.dtype,device=device).scatter_add(0, segment_ids, ones_data)
+    tensor = tensor / tensor_nums
+    return tensor
+
+def unsorted_segment_max(x, segment_ids, num_segments=None):
+    if num_segments is not None:
+        assert segment_ids.max() < num_segments
+
+    assert x.shape[0] == segment_ids.shape[0], "the length of segment_ids should be equal to data.shape[0]."
+    res = []
+    for i in range(num_segments):
+        res.append(torch.max(x[segment_ids == i], dim=0)[0])
+    return torch.stack(res, dim=0)
 
 
 
@@ -46,7 +77,9 @@ pf = Profiler()
 pf.start()
 for j in range(1000):
     msg = tlx.gather(x, src)
-    unsorted_segment_sum(msg, dst, num_nodes)
+    # unsorted_segment_sum(msg, dst, num_nodes)
+    unsorted_segment_mean(msg, dst, num_nodes)
+    # unsorted_segment_max(msg, dst, num_nodes)
 pf.stop()
 print(pf.output_text(unicode=True, color=True))
 
@@ -59,6 +92,8 @@ dst = tlx.gather(dst, idx)
 pf.start()
 for j in range(1000):
     msg = tlx.gather(x, src)
-    unsorted_segment_sum(msg, dst, num_nodes)
+    # unsorted_segment_sum(msg, dst, num_nodes)
+    unsorted_segment_mean(msg, dst, num_nodes)
+    # unsorted_segment_max(msg, dst, num_nodes)
 pf.stop()
 print(pf.output_text(unicode=True, color=True))
