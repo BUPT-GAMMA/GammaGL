9ec2956961113fe930955411931e753af111d7b6
zsy0828 <48177103+zsy0828@users.noreply.github.com>
2022-04-12 15:50:55 +0800

    修改set up，支持直接pip安装使用
    直接from gammagl import sample.

--- gammagl/__init__.py
+++ gammagl/__init__.py
@@ -1,5 +1,6 @@
 # from gammagl import data, datasets, layers, models, utils, sparse
 # from gammagl.data.graph import Graph
 # from gammagl.sparse.sparse_adj import SparseAdj
+from gammagl import sample
 
 __version__ = '0.0.1'
