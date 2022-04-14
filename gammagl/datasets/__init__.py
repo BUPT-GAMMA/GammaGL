4910b726e5d6f4d277d2c99b61b2c1a84b549907
Theheavens <34649403+Theheavens@users.noreply.github.com>
2022-04-14 20:52:08 +0800

    update dataset
    1. add entities dataset

--- gammagl/datasets/__init__.py
+++ gammagl/datasets/__init__.py
@@ -1,6 +1,5 @@
-# from .cora import Cora
-# from .citeseer import CiteSeer
 from .tu_dataset import TUDataset
 from .planetoid import Planetoid
 from .reddit import Reddit
 from .imdb import IMDB
+from .entities import Entities
