3a1a6db884d245eb3e871573b934f545a1cd965c
Coldeye <948283368@qq.com>
2022-05-10 23:10:51 +0800

    tf result

--- examples/gprgnn/Reproduce_GPRGNN.sh
+++ examples/gprgnn/Reproduce_GPRGNN.sh
@@ -55,29 +55,29 @@
 # python process.py
 
 # Below is for heterophily datasets, dense split
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-    python gprgnn_trainer.py --train_rate 0.6 \
-            --val_rate 0.2 \
-            --dataset chameleon \
-            --lr 0.05 \
-            --alpha 1.0 \
-            --l2_coef 0.0 \
-            --dprate 0.7 
-done
-python process.py
+# for i in 1 2 3 4 5 6 7 8 9 10
+# do
+#     python gprgnn_trainer.py --train_rate 0.6 \
+#             --val_rate 0.2 \
+#             --dataset chameleon \
+#             --lr 0.05 \
+#             --alpha 1.0 \
+#             --l2_coef 0.0 \
+#             --dprate 0.7 
+# done
+# python process.py
 
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-    python gprgnn_trainer.py --train_rate 0.6 \
-            --val_rate 0.2 \
-            --dataset squirrel \
-            --lr 0.05 \
-            --alpha 0.0 \
-            --dprate 0.7 \
-            --l2_coef 0.0
-done
-python process.py
+# for i in 1 2 3 4 5 6 7 8 9 10
+# do
+#     python gprgnn_trainer.py --train_rate 0.6 \
+#             --val_rate 0.2 \
+#             --dataset squirrel \
+#             --lr 0.05 \
+#             --alpha 0.0 \
+#             --dprate 0.7 \
+#             --l2_coef 0.0
+# done
+# python process.py
 
 # for i in 1 2 3 4 5 6 7 8 9 10
 # do
@@ -89,12 +89,12 @@ python process.py
 # done
 # python process.py
 
-# for i in 1 2 3 4 5 6 7 8 9 10
-# do       
-#     python gprgnn_trainer.py --train_rate 0.6 \
-#             --val_rate 0.2 \
-#             --dataset cornell \
-#             --lr 0.05 \
-#             --alpha 0.9 
-# done
-# python process.py
+for i in 1 2 3 4 5 6 7 8 9 10
+do       
+    python gprgnn_trainer.py --train_rate 0.6 \
+            --val_rate 0.2 \
+            --dataset cornell \
+            --lr 0.05 \
+            --alpha 0.9 
+done
+python process.py
--- examples/gprgnn/process.py
+++ examples/gprgnn/process.py
@@ -3,7 +3,8 @@ import numpy as np
 import argparse
 
 with open('./test_accuracy', 'r+') as f:
-    data = f.read().split('\n')[-10:-1]
+    # data = f.read().split('\n')[-10:-1]
+    data = f.read().split('\n')[12:22] 
     acc = np.array([r.split(' ')[2] for r in data], dtype=np.float32)
     f.write("{} {} {:.2f} {:.2f}\n\n".format(data[0].split(' ')[0], data[0].split(' ')[1], acc.mean(), acc.std()))
 
