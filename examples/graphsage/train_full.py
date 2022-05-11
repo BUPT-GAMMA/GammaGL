1dd23e8e1979684fd920396f0efba00147530e9d
zsy0828 <48177103+zsy0828@users.noreply.github.com>
2022-05-11 17:38:58 +0800

    [bug] fix
    小修改，将readme，以及trainer修改

--- examples/graphsage/train_full.py
+++ examples/graphsage/train_full.py
@@ -1,7 +1,8 @@
-import os
-
+# import os
+# os.environ['TL_BACKEND'] = 'paddle'
+# os.environ['CUDA_VISIBLE_DEVICES'] = ' '
 from gammagl.models.graphsage import GraphSAGE_Full_Model
-import tensorflow as tf
+
 import argparse
 import tensorlayerx as tlx
 from gammagl.datasets import Planetoid
@@ -18,9 +19,7 @@ class SemiSpvzLoss(WithLoss):
         train_logits = logits[data['train_mask']]
         train_label = label[data['train_mask']]
         loss = self._loss_fn(train_logits, train_label)
-        l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in
-                            self._backbone.trainable_weights]) * args.l2_coef  # only support for tensorflow backend
-        return loss + l2_loss
+        return loss
 
 
 def evaluate(net, data, y, mask, metrics):
@@ -51,7 +50,7 @@ def main(args):
                                n_classes=graph.y.shape[1],
                                n_layers=args.n_layers,
                                activation=tlx.ReLU(),
-                               dropout=1 - args.keep_rate,
+                               dropout=args.drop_rate,
                                aggregator_type=args.aggregator_type)
     optimizer = tlx.optimizers.Adam(args.lr)
     metrics = tlx.metrics.Accuracy()
@@ -63,7 +62,7 @@ def main(args):
     data = {
         "x": x,
         "edge_index": edge_index,
-        "edge_weight": tlx.ones(edge_index.shape[1], dtype=tlx.float32),
+        "edge_weight": tlx.ones((edge_index.shape[1], ), dtype=tlx.float32),
         "train_mask": graph.train_mask,
         "test_mask": graph.test_mask,
         "val_mask": graph.val_mask,
@@ -77,7 +76,7 @@ def main(args):
         val_acc = evaluate(net, data, y, data['val_mask'], metrics)
 
         print("Epoch [{:0>3d}] ".format(epoch + 1) \
-              + "  train loss: {:.4f}".format(train_loss) \
+              + "  train loss: {:.4f}".format(train_loss.item()) \
               + "  val acc: {:.4f}".format(val_acc))
 
         # save best model on evaluation set
@@ -96,7 +95,7 @@ if __name__ == '__main__':
     parser.add_argument("--lr", type=float, default=0.01, help="learnin rate")
     parser.add_argument("--n_epoch", type=int, default=200, help="number of epoch")
     parser.add_argument("--hidden_dim", type=int, default=16, help="dimention of hidden layers")
-    parser.add_argument("--keep_rate", type=float, default=0.5, help="keep_rate = 1 - drop_rate")
+    parser.add_argument("--drop_rate", type=float, default=0.5, help="drop_rate")
     parser.add_argument("--l2_coef", type=float, default=5e-4, help="l2 loss coeficient")
     parser.add_argument('--dataset', type=str, default='cora', help='dataset')
     parser.add_argument("--dataset_path", type=str, default=r'../', help="path to save dataset")
