404111bb5d5a72ed8325af40b6db9369e51af91f
dddg617 <996179900@qq.com>
2022-05-13 15:45:29 +0800

    [Docs]fix docs bugs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, BUPT-GAMMA LAB'
 author = 'BUPT-GAMMA LAB'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
