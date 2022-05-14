868d6071ebade649326dc8b4288eeb8e87fb1149
zzy_wizard <github 710697495@qq.com>
2022-05-14 12:12:44 +0800

    first commit
    add coalesce.py
    update loop.py
    add undirected.py

--- gammagl/utils/loop.py
+++ gammagl/utils/loop.py
@@ -73,3 +73,19 @@ def add_self_loops(
 
     edge_index = tlx.concat([edge_index, loop_index], axis=1)
     return edge_index, edge_attr
+
+def remove_self_loops(edge_index, edge_attr=None):
+    r"""Removes every self-loop in the graph given by :attr:`edge_index`, so
+    that :math:`(i,i) \not\in \mathcal{E}` for every :math:`i \in \mathcal{V}`.
+    Args:
+        edge_index (LongTensor): The edge indices.
+        edge_attr (Tensor, optional): Edge weights or multi-dimensional
+            edge features. (default: :obj:`None`)
+    :rtype: (:class:`LongTensor`, :class:`Tensor`)
+    """
+    mask = edge_index[0] != edge_index[1]
+    edge_index = edge_index[:, mask]
+    if edge_attr is None:
+        return edge_index, None
+    else:
+        return edge_index, edge_attr[mask]
