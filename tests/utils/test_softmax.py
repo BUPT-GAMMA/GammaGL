f19fab4ac6fdae5df973cd5c9e4a6f8421613441
hanhui <193691140@qq.com>
2022-05-16 12:15:56 +0800

    [dataset] plantoid split

--- tests/utils/test_softmax.py
+++ tests/utils/test_softmax.py
@@ -16,7 +16,7 @@ import tensorlayerx as tlx
 import numpy as np
 from gammagl.utils.softmax import segment_softmax
 
-def test():
+def test_softmax():
     x = tlx.convert_to_tensor([[1,1], [1,1], [2,4], [2,4]], dtype=tlx.float32)
     edge_index = tlx.convert_to_tensor([[0, 1, 2, 2, 3],
                                         [1, 2, 1, 0, 3]])
@@ -25,4 +25,4 @@ def test():
 
     assert abs(sum(tlx.convert_to_numpy(score[2]) - np.array([0.7311, 0.9526]))) < 1e-4
 
-test()
+test_softmax()
