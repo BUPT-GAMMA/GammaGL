791b519c4b6a2d291f4319a294ecd77a3cdacb9f
Theheavens <34649403+Theheavens@users.noreply.github.com>
2022-05-22 00:00:15 +0800

    Update test_tu.py

--- tests/datasets/test_tu.py
+++ tests/datasets/test_tu.py
@@ -2,6 +2,6 @@
 def test_ENZYMES(get_dataset):
     dataset = get_dataset(name='ENZYMES')
     assert len(dataset) == 600
-    assert len(dataset.num_classes) == 6
+    assert dataset.num_classes == 6
     assert dataset.num_node_features == 3
 
