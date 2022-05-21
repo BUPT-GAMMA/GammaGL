b1c1fc89e11a516ded35211d0aec1351cb849989
BUPT-SSE-KAKA <1048435989@qq.com>
2022-05-21 13:24:04 +0800

    add fagcn test

--- tests/layers/conv/test_fagcn_conv.py
+++ tests/layers/conv/test_fagcn_conv.py
@@ -12,20 +12,18 @@ os.environ['TL_BACKEND'] = 'torch'
 # os.environ['TL_BACKEND'] = 'paddle'
 # os.environ['TL_BACKEND'] = 'mindspore'
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
+import numpy as np
 import tensorlayerx as tlx
-from gammagl.layers.conv import MessagePassing
+from gammagl.layers.conv import FAGCNConv
+
 
 def test_fagcn():
-    x = tlx.reshape(tlx.arange(0, 8, dtype=tlx.float32), shape=(4,2))
-    edge_index = tlx.convert_to_tensor([[0, 1, 2, 2, 3],
-                                        [1, 2, 1, 0, 3]])
+    x = tlx.random_uniform(shape=(4, 16))
+    edge_index = tlx.convert_to_tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    src_degree = tlx.convert_to_tensor(np.power([3, 3, 3, 1, 1, 1], -0.5), dtype='float32')
+    dst_degree = tlx.convert_to_tensor(np.power([1, 1, 1, 3, 3, 3], -0.5), dtype='float32')
 
-    mp = MessagePassing()
-    sum_x = mp.propagate(x, edge_index, aggr='sum')
-    mean_x = mp.propagate(x, edge_index, aggr='mean')
-    max_x = mp.propagate(x, edge_index, aggr='max')
+    conv = FAGCNConv(src_degree=src_degree, dst_degree=dst_degree, hidden_dim=16, drop_rate=0.6)
+    out = conv(x, edge_index, x.shape[0])
 
-    assert tlx.ops.convert_to_numpy(sum_x).tolist() == [[4.0, 5.0], [4.0, 6.0], [2.0, 3.0], [6.0, 7.0]]
-    assert tlx.ops.convert_to_numpy(mean_x).tolist() == [[4.0, 5.0], [2.0, 3.0], [2.0, 3.0], [6.0, 7.0]]
-    assert tlx.ops.convert_to_numpy(max_x).tolist() == [[4.0, 5.0], [4.0, 5.0], [2.0, 3.0], [6.0, 7.0]]
+    assert out.shape == (4, 16)
