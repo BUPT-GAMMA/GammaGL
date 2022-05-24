53aa769ee74c6f0632ab64008eaf1e4861f4f237
aurora-xin <2398486514@qq.com>
2022-05-24 15:36:32 +0800

    update
    delete redundant code, merge graph sampler into dd.py

--- gammagl/datasets/dd.py
+++ gammagl/datasets/dd.py
@@ -1,31 +1,17 @@
 import networkx as nx
 import numpy as np
-import scipy as sc
 import os
 import re
 import tensorlayerx as tlx
-# import torch
-
-import pickle
 import random
 
-from gammagl.data.graph_sampler import GraphSampler
 
-#import util
-# ---- NetworkX compatibility
 def node_iter(G):
-    # if float(nx.__version__)<2.0:
-    #     return G.nodes()
-    # else:
     return G.nodes
 
 def node_dict(G):
-    #if float(nx.__version__)>2.1:
-    node_dict = G.nodes #159
-    # else:
-    #     node_dict = G.node
+    node_dict = G.nodes
     return node_dict
-# ---------------------------
 
 def exp_moving_avg(x, decay=0.9):
     shadow = x[0]
@@ -34,6 +20,114 @@ def exp_moving_avg(x, decay=0.9):
         shadow -= (1-decay) * (shadow-v)
         a.append(shadow)
     return a
+
+class GraphSampler(tlx.dataflow.Dataset):
+    ''' Sample graphs and nodes in graph
+    '''
+
+    def __init__(self, G_list, features='default', normalize=True, assign_feat='default', max_num_nodes=0):
+        self.adj_all = []
+        self.len_all = []
+        self.feature_all = []
+        self.label_all = []
+
+        self.assign_feat_all = []
+
+        if max_num_nodes == 0:
+            self.max_num_nodes = max([G.number_of_nodes() for G in G_list])
+        else:
+            self.max_num_nodes = max_num_nodes  # 1000
+
+        if features == 'default':
+            self.feat_dim = node_dict(G_list[0])[0]['feat'].shape[0]  # 89
+
+        # G_list 1052
+        for G in G_list:
+            adj = np.array(nx.to_numpy_matrix(G))  # 159，159
+            if normalize:
+                sqrt_deg = np.diag(1.0 / np.sqrt(np.sum(adj, axis=0, dtype=float).squeeze()))
+                adj = np.matmul(np.matmul(sqrt_deg, adj), sqrt_deg)
+            self.adj_all.append(adj)
+            self.len_all.append(G.number_of_nodes())
+            self.label_all.append(G.graph['label'])
+            # feat matrix: max_num_nodes x feat_dim
+            if features == 'default':
+                f = np.zeros((self.max_num_nodes, self.feat_dim), dtype=float)  # 1000，89
+                for i, u in enumerate(G.nodes()):  # 314
+                    f[i, :] = node_dict(G)[u]['feat']
+                self.feature_all.append(f)  # 1000，89
+            elif features == 'id':
+                self.feature_all.append(np.identity(self.max_num_nodes))
+            elif features == 'deg-num':
+                degs = np.sum(np.array(adj), 1)
+                degs = np.expand_dims(np.pad(degs, [0, self.max_num_nodes - G.number_of_nodes()], 0),
+                                      axis=1)
+                self.feature_all.append(degs)
+            elif features == 'deg':
+                self.max_deg = 10
+                degs = np.sum(np.array(adj), 1).astype(int)
+                degs[degs > self.max_deg] = self.max_deg
+                feat = np.zeros((len(degs), self.max_deg + 1))
+                feat[np.arange(len(degs)), degs] = 1
+                feat = np.pad(feat, ((0, self.max_num_nodes - G.number_of_nodes()), (0, 0)),
+                              'constant', constant_values=0)
+
+                f = np.zeros((self.max_num_nodes, self.feat_dim), dtype=float)
+                for i, u in enumerate(node_iter(G)):
+                    f[i, :] = node_dict(G)[u]['feat']
+
+                feat = np.concatenate((feat, f), axis=1)
+
+                self.feature_all.append(feat)
+            elif features == 'struct':
+                self.max_deg = 10
+                degs = np.sum(np.array(adj), 1).astype(int)
+                degs[degs > 10] = 10
+                feat = np.zeros((len(degs), self.max_deg + 1))
+                feat[np.arange(len(degs)), degs] = 1
+                degs = np.pad(feat, ((0, self.max_num_nodes - G.number_of_nodes()), (0, 0)),
+                              'constant', constant_values=0)
+
+                clusterings = np.array(list(nx.clustering(G).values()))
+                clusterings = np.expand_dims(np.pad(clusterings,
+                                                    [0, self.max_num_nodes - G.number_of_nodes()],
+                                                    'constant'),
+                                             axis=1)
+                g_feat = np.hstack([degs, clusterings])
+                if 'feat' in node_dict(G)[0]:
+                    node_feats = np.array([node_dict(G)[i]['feat'] for i in range(G.number_of_nodes())])
+                    node_feats = np.pad(node_feats, ((0, self.max_num_nodes - G.number_of_nodes()), (0, 0)),
+                                        'constant')
+                    g_feat = np.hstack([g_feat, node_feats])
+
+                self.feature_all.append(g_feat)
+
+            if assign_feat == 'id':
+                self.assign_feat_all.append(
+                    np.hstack((np.identity(self.max_num_nodes), self.feature_all[-1])))
+            else:
+                self.assign_feat_all.append(self.feature_all[-1])
+
+        self.feat_dim = self.feature_all[0].shape[1]
+        self.assign_feat_dim = self.assign_feat_all[0].shape[1]
+
+    def __len__(self):
+        return len(self.adj_all)
+
+    def __getitem__(self, idx):
+        adj = self.adj_all[idx]
+        num_nodes = adj.shape[0]
+        adj_padded = np.zeros((self.max_num_nodes, self.max_num_nodes))  # 1000,1000
+        adj_padded[:num_nodes, :num_nodes] = adj
+
+        # use all nodes for aggregation (baseline)
+
+        return {'adj': adj_padded,
+                'feats': self.feature_all[idx].copy(),
+                'label': self.label_all[idx],
+                'num_nodes': num_nodes,
+                'assign_feats': self.assign_feat_all[idx].copy()}
+
 
 def read_graphfile(datadir, dataname, max_nodes=None):
     ''' Read data from https://ls11-www.cs.tu-dortmund.de/staff/morris/graphkerneldatasets
@@ -137,54 +231,36 @@ def read_graphfile(datadir, dataname, max_nodes=None):
         for n in node_iter(G):
             mapping[n] = it
             it += 1
-
         # indexed from 0
         graphs.append(nx.relabel_nodes(G, mapping))
     return graphs
 
 
 def prepare_val_data(graphs, args, val_idx, max_nodes=0):
-# DD 1168
     random.shuffle(graphs)
-    val_size = len(graphs) // 10 #116
-    train_graphs = graphs[:val_idx * val_size] #1052
+    val_size = len(graphs) // 10
+    train_graphs = graphs[:val_idx * val_size]
     if val_idx < 9:
         train_graphs = train_graphs + graphs[(val_idx+1) * val_size :]
-    val_graphs = graphs[val_idx*val_size: (val_idx+1)*val_size] #116
-    print('Num training graphs: ', len(train_graphs),
-          '; Num validation graphs: ', len(val_graphs))
-
-    print('Number of graphs: ', len(graphs))
-    print('Number of edges: ', sum([G.number_of_edges() for G in graphs]))
-    print('Max, avg, std of graph size: ',
-            max([G.number_of_nodes() for G in graphs]), ', '
-            "{0:.2f}".format(np.mean([G.number_of_nodes() for G in graphs])), ', '
-            "{0:.2f}".format(np.std([G.number_of_nodes() for G in graphs])))
-
-# #Using node labels
-# Num training graphs:  1052 ; Num validation graphs:  116
-# Number of graphs:  1168
-# Number of edges:  789819
-#Max, avg, std of graph size:  903 , 268.70 , 161.33
+    val_graphs = graphs[val_idx*val_size: (val_idx+1)*val_size]
 
     # minibatch
     dataset_sampler = GraphSampler(train_graphs, normalize=False, max_num_nodes=max_nodes,
-            features=args.feature_type) #adj_all1052; feat_all 1052; assign_feat_dim 1052; feat_dim 89
+            features=args.feature_type)
 
     train_dataset_loader = tlx.dataflow.DataLoader(
             dataset_sampler,
-            batch_size=args.batch_size, #20 #batch_sampler53
+            batch_size=args.batch_size,
             shuffle=True,
             num_workers=args.num_workers)
 
     dataset_sampler = GraphSampler(val_graphs, normalize=False, max_num_nodes=max_nodes,
-            features=args.feature_type) #116,89
+            features=args.feature_type)
     val_dataset_loader = tlx.dataflow.DataLoader(
             dataset_sampler,
-            batch_size=args.batch_size, #20 #6
+            batch_size=args.batch_size,
             shuffle=False,
             num_workers=args.num_workers)
-#53,6
     return train_dataset_loader, val_dataset_loader, \
             dataset_sampler.max_num_nodes, dataset_sampler.feat_dim, dataset_sampler.assign_feat_dim #89.89
 
