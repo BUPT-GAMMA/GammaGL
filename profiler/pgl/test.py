f6008f0660c25f0c0637df666bbdb291334a81c2
hanhui <193691140@qq.com>
2022-05-24 15:23:32 +0800

    update test script

--- profiler/pgl/test.py
+++ profiler/pgl/test.py
@@ -5,10 +5,40 @@
 # @Author  : clear
 # @FileName: test.py.py
 
+
 from subprocess import run
+import os
 
-for i in range(5):
-    run("CUDA_VISIBLE_DEVICES=1 python pgl_gcn.py >> log.txt", shell=True)
-for i in range(5):
-    run("CUDA_VISIBLE_DEVICES=1 python pgl_gat.py >> log.txt", shell=True)
+logfile = 'log.txt'
+gpu = 8
+iter = 5
+dataset = 'cora'
 
+if os.path.exists('log.txt'):
+    os.remove('log.txt')
+
+for i in range(iter):
+    run("CUDA_VISIBLE_DEVICES={} python pgl_gcn.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+for i in range(iter):
+    run("CUDA_VISIBLE_DEVICES={} python pgl_gat.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+
+
+aa = []
+bb = []
+cc = []
+with open(logfile) as f:
+    lines = f.readlines()
+    counter = 0
+    for line in lines:
+        a, b, c = line.split()
+        aa.append(float(a))
+        bb.append(float(b))
+        cc.append(float(c))
+        counter += 1
+        if counter % iter == 0:
+            print(sum(aa)/iter, sum(bb)/iter, sum(cc)/iter)
+            aa = []
+            bb = []
+            cc = []
