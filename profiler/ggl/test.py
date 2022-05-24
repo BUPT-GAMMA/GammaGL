959ba98175c22a72d2c317dc7be7406c492060e8
hanhui <193691140@qq.com>
2022-05-24 15:23:32 +0800

    update test script

--- profiler/ggl/test.py
+++ profiler/ggl/test.py
@@ -6,21 +6,58 @@
 # @FileName: test.py.py
 
 from subprocess import run
+import os
 
-for i in range(5):
-    run("TL_BACKEND=torch CUDA_VISIBLE_DEVICES=1 python gcn_trainer.py >> log.txt", shell=True)
-for i in range(5):
-    run("TL_BACKEND=mindspore CUDA_VISIBLE_DEVICES=1 python gcn_trainer.py >> log.txt", shell=True)
-for i in range(5):
-    run("TL_BACKEND=tensorflow CUDA_VISIBLE_DEVICES=1 python gcn_trainer.py >> log.txt", shell=True)
-for i in range(5):
-    run("TL_BACKEND=paddle CUDA_VISIBLE_DEVICES=1 python gcn_trainer.py >> log.txt", shell=True)
+logfile = 'log.txt'
+gpu = 8
+iter = 5
+dataset = 'pubmed'
 
-for i in range(5):
-    run("TL_BACKEND=torch CUDA_VISIBLE_DEVICES=1 python gat_trainer.py >> log.txt", shell=True)
-for i in range(5):
-    run("TL_BACKEND=mindspore CUDA_VISIBLE_DEVICES=1 python gat_trainer.py >> log.txt", shell=True)
-for i in range(5):
-    run("TL_BACKEND=tensorflow CUDA_VISIBLE_DEVICES=1 python gat_trainer.py >> log.txt", shell=True)
-for i in range(5):
-    run("TL_BACKEND=paddle CUDA_VISIBLE_DEVICES=1 python gat_trainer.py >> log.txt", shell=True)
+if os.path.exists('log.txt'):
+    os.remove('log.txt')
+
+# GCN
+for i in range(iter):
+    run("TL_BACKEND=torch CUDA_VISIBLE_DEVICES={} python gcn_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+for i in range(iter):
+    run("TL_BACKEND=mindspore CUDA_VISIBLE_DEVICES={} python gcn_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+for i in range(iter):
+    run("TL_BACKEND=tensorflow CUDA_VISIBLE_DEVICES={} python gcn_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+for i in range(iter):
+    run("TL_BACKEND=paddle CUDA_VISIBLE_DEVICES={} python gcn_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+
+# GAT
+for i in range(iter):
+    run("TL_BACKEND=torch CUDA_VISIBLE_DEVICES={} python gat_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+for i in range(iter):
+    run("TL_BACKEND=mindspore CUDA_VISIBLE_DEVICES={} python gat_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+for i in range(iter):
+    run("TL_BACKEND=tensorflow CUDA_VISIBLE_DEVICES={} python gat_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+for i in range(iter):
+    run("TL_BACKEND=paddle CUDA_VISIBLE_DEVICES={} python gat_trainer.py --dataset {} >> {}"
+        .format(gpu, dataset, logfile), shell=True)
+
+aa = []
+bb = []
+cc = []
+with open(logfile) as f:
+    lines = f.readlines()
+    counter = 0
+    for line in lines:
+        a, b, c = line.split()
+        aa.append(float(a))
+        bb.append(float(b))
+        cc.append(float(c))
+        counter += 1
+        if counter % iter == 0:
+            print(sum(aa)/iter, sum(bb)/iter, sum(cc)/iter)
+            aa = []
+            bb = []
+            cc = []
