06ba172ca2a0b61988106a65e2b6a347602cb35e
Theheavens <34649403+Theheavens@users.noreply.github.com>
2022-06-20 11:43:56 +0800

    [docs] fix
    `torch_geometric` to `gammagl

--- gammagl/datasets/shapenet.py
+++ gammagl/datasets/shapenet.py
@@ -43,15 +43,15 @@ class ShapeNet(InMemoryDataset):
             If :obj:`"test"`, loads the test dataset.
             (default: :obj:`"trainval"`)
         transform (callable, optional): A function/transform that takes in an
-            :obj:`torch_geometric.data.Data` object and returns a transformed
+            :obj:`gammagl.data.Graph` object and returns a transformed
             version. The data object will be transformed before every access.
             (default: :obj:`None`)
         pre_transform (callable, optional): A function/transform that takes in
-            an :obj:`torch_geometric.data.Data` object and returns a
+            an :obj:`gammagl.data.Graph` object and returns a
             transformed version. The data object will be transformed before
             being saved to disk. (default: :obj:`None`)
         pre_filter (callable, optional): A function that takes in an
-            :obj:`torch_geometric.data.Data` object and returns a boolean
+            :obj:`gammagl.data.Graph` object and returns a boolean
             value, indicating whether the data object should be included in the
             final dataset. (default: :obj:`None`)
     """
