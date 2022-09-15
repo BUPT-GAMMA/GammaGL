25b87b90da0ec8d2e99c823395d9dfc99b1c12cf
ZZ NAN <57925162+hhiuhu338@users.noreply.github.com>
2022-09-15 17:26:43 +0800

    Update __init__.py

--- gammagl/transforms/__init__.py
+++ gammagl/transforms/__init__.py
@@ -4,7 +4,7 @@ from .add_metapaths import AddMetaPaths
 from .compose import Compose
 from .sign import SIGN
 from .normalize_features import NormalizeFeatures
-from .DropEdge import DropEdge
+from .drop_edge import DropEdge
 
 __all__ = [
     'BaseTransform',
