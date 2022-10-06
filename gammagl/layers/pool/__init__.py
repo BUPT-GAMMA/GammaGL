645b138066c655f1d0d5d6b7c25740de3ebbd4ab
ddstkm <742003551@qq.com>
2022-10-06 21:15:56 +0800

    to_dense_batch pass; global_sort_pool pass;

--- gammagl/layers/pool/__init__.py
+++ gammagl/layers/pool/__init__.py
@@ -1,11 +1,13 @@
 from .glob import global_max_pool
 from .glob import global_mean_pool
 from .glob import global_sum_pool
+from .glob import global_sort_pool
 
 __all__ = [
     'global_max_pool',
     'global_mean_pool',
-    'global_sum_pool'
+    'global_sum_pool',
+    'global_sort_pool'
 ]
 
 classes = __all__
--- gammagl/utils/__init__.py
+++ gammagl/utils/__init__.py
@@ -9,6 +9,7 @@ from .loop import add_self_loops, remove_self_loops, contains_self_loops
 from .mask import mask_to_index, index_to_mask
 from .inspector import Inspector
 from .device import set_device
+from .to_dense_batch import to_dense_batch
 
 __all__ = [
     'calc_A_norm_hat',
@@ -23,7 +24,8 @@ __all__ = [
     'remove_self_loops',
     'mask_to_index',
     'index_to_mask',
-    'set_device'
+    'set_device',
+    'to_dense_batch'
 ]
 
 classes = __all__
