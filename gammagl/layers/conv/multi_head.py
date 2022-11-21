9b2a3d5fc6b10db1bef635270cb1c19ac1a87454
starry_sky <yangcd@bupt.edu.cn>
2022-11-21 17:00:20 +0800

    Update multi_head.py

--- gammagl/layers/conv/multi_head.py
+++ gammagl/layers/conv/multi_head.py
@@ -31,7 +31,7 @@ class MultiHead(MessagePassing):
                   \mathbf{x}^{\prime}_i = \beta_i \mathbf{W}_1 \mathbf{x}_i +
                   (1 - \beta_i) \underbrace{\left(\sum_{j \in \mathcal{N}(i)}
                   \alpha_{i,j} \mathbf{W}_2 \vec{x}_j \right)}_{=\mathbf{m}_i}
-
+          beta
               with :math:`\beta_i = \textrm{sigmoid}(\mathbf{w}_5^{\top}
               [ \mathbf{W}_1 \mathbf{x}_i, \mathbf{m}_i, \mathbf{W}_1
               \mathbf{x}_i - \mathbf{m}_i ])` (default: :obj:`False`)
@@ -52,24 +52,21 @@ class MultiHead(MessagePassing):
                   {\sqrt{d}} \right)
     """
 
-    def __init__(self, in_features, out_features, n_heads,num_nodes):
+    def __init__(self, in_features, out_features, n_heads,num_nodes,beta=True):
         super().__init__()
+        self.beta=beta
         self.heads=n_heads
         self.num_nodes=num_nodes
         self.out_channels=out_features
         self.linear = tlx.layers.Linear(out_features=out_features* n_heads,
                                         in_features=in_features)
 
-        self.lin_key = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features)
-        self.lin_query = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features)
-        self.lin_value = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features)
-        self.lin_skip = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features)
-        init = tlx.initializers.RandomNormal()
-        self.att_src = init(shape=(1, n_heads, out_features), dtype=tlx.float32)
-        self.att_dst = init(shape=(1, n_heads, out_features), dtype=tlx.float32)
-
-        self.leaky_relu = tlx.layers.LeakyReLU(0.2)
-        self.dropout = tlx.layers.Dropout()
+        self.lin_key = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features, bias=True)
+        self.lin_query = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features, bias=True)
+        self.lin_value = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features, bias=True)
+        self.lin_skip = tlx.layers.Linear(in_features=in_features, out_features=n_heads * out_features, bias=True)
+        if beta:
+            self.lin_beta = tlx.layers.Linear(3 * n_heads * out_features, 1, bias=False)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -77,6 +74,8 @@ class MultiHead(MessagePassing):
         self.lin_query.reset_parameters()
         self.lin_value.reset_parameters()
         self.lin_skip.reset_parameters()
+        if self.beta:
+            self.lin_beta.reset_parameters()
 
     def message(self,  query, key, value):
         alpha = (query * key).sum(dim=-1) / math.sqrt(self.out_channels)
@@ -93,4 +92,9 @@ class MultiHead(MessagePassing):
         value = self.lin_value(x[0]).view(-1, H, C)
         out = self.propagate(edge_index, query=query, key=key, value=value)
         out = out.view(-1, self.heads * self.out_channels)
+        if self.beta:
+            x_r = self.lin_skip(x[1])
+            beta = self.lin_beta(tlx.ops.concat([out, x_r, out - x_r], aixs=-1))
+            beta = beta.sigmoid()
+            out = beta * x_r + (1 - beta) * out
         return out
