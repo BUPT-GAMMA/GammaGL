e8b4b7a932afe4348fe40a5b7ab27dbe07599dd9
WuLuoC <1780570778@qq.com>
2022-12-19 14:02:39 +0800

    rename random_link_split to vgae_pre.

--- gammagl/transforms/__init__.py
+++ gammagl/transforms/__init__.py
@@ -4,7 +4,7 @@ from .compose import Compose
 from .sign import SIGN
 from .normalize_features import NormalizeFeatures
 from .drop_edge import DropEdge
-from .random_link_split import mask_test_edges, sparse_to_tuple
+from .vgae_pre import mask_test_edges, sparse_to_tuple
 
 __all__ = [
     'BaseTransform',
