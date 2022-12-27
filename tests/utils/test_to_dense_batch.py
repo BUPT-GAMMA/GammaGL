02b8215e791bcc2b63f74a7cdfa3ca3728772168
Tianyu Zhao <34649403+Theheavens@users.noreply.github.com>
2022-12-27 10:32:11 +0800

    Update test_to_dense_batch.py

--- tests/utils/test_to_dense_batch.py
+++ tests/utils/test_to_dense_batch.py
@@ -2,7 +2,6 @@ import tensorlayerx as tlx
 from gammagl.utils import to_dense_batch
 
 def test_to_dense_batch():
-    tlx.set_device()
     x = tlx.convert_to_tensor([[1,2], [3,4], [5,6], [6,7]])
     batch = tlx.convert_to_tensor([0, 1, 1, 1])
     a, b = to_dense_batch(x, batch)
