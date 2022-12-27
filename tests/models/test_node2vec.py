dc6b869b9db41debee2afe61539ec9828982a263
dddg617 <996179900@qq.com>
2022-12-27 02:21:17 +0000

    [Test]Fix test

--- tests/models/test_node2vec.py
+++ tests/models/test_node2vec.py
@@ -9,7 +9,7 @@ def test_node2vec():
     model = Node2vecModel(edge_index=edge_index, embedding_dim=16, walk_length=2,
                           num_walks=1, p=1.0, q=1.0, edge_weight=None, window_size=2)
 
-    assert model.campute()[0].shape == (3, 16)
+    assert tlx.get_tensor_shape(model.campute()[0]) == [3, 16]
 
     pos_rw = model.pos_sample()
     neg_rw = model.neg_sample()
