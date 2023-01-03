d98f27bd8d0c8ae8578d46d618ad661ddf39c715
theheavens <theheavenszhao@outlook.com>
2023-01-03 20:49:31 +0800

    [bug]donwload
    https://bugs.python.org/issue42853
    Mentioned in (PyG)[https://github.com/pyg-team/pytorch_geometric/commit/cd6c1f7179fc28e953e98be0eaf8aa5f201ca94d]

--- gammagl/data/download.py
+++ gammagl/data/download.py
@@ -1,12 +1,15 @@
 import os
+import os.path as osp
 import ssl
 import sys
-import urllib.request
-import os.path as osp
+import urllib
+from typing import Optional
+
 from .makedirs import makedirs
 
 
-def download_url(url: str, folder: str, log: bool = True):
+def download_url(url: str, folder: str, log: bool = True,
+                 filename: Optional[str] = None):
     r"""Downloads the content of an URL to a specific folder.
 
     Args:
@@ -16,12 +19,12 @@ def download_url(url: str, folder: str, log: bool = True):
             console. (default: :obj:`True`)
     """
 
-    filename = url.rpartition('/')[2]
-    filename = filename if filename[0] == '?' else filename.split('?')[0]
-    path = osp.join(folder, filename)
-
+    if filename is None:
+        filename = url.rpartition('/')[2]
+        filename = filename if filename[0] == '?' else filename.split('?')[0]
     if os.environ.get('GGL_GITHUB_PROXY') == 'TRUE' and ('raw.githubusercontent.com' in url or 'github.com' in url):
         url = 'https://ghproxy.com/' + url
+    path = osp.join(folder, filename)
 
     if osp.exists(path):  # pragma: no cover
         if log:
@@ -37,6 +40,11 @@ def download_url(url: str, folder: str, log: bool = True):
     data = urllib.request.urlopen(url, context=context)
 
     with open(path, 'wb') as f:
-        f.write(data.read())
+        # workaround for https://bugs.python.org/issue42853
+        while True:
+            chunk = data.read(10 * 1024 * 1024)
+            if not chunk:
+                break
+            f.write(chunk)
 
-    return path
+    return 
