362a159197df8460703909194afcbf0499ed51f1
hanhui <193691140@qq.com>
2023-01-13 13:12:32 +0800

    "circul import"

--- profiler/mpops/paddle_ext_test.py
+++ profiler/mpops/paddle_ext_test.py
@@ -1,5 +1,5 @@
 import paddle
-from paddle_segment import unsorted_segment_sum
+from paddle_ext import unsorted_segment_sum
 
 src = paddle.to_tensor([[1, 1], [2, 2], [3, 3]], dtype='float32', stop_gradient=False)
 ## TODO: it still successfully run, but it will get wrong answer. 
