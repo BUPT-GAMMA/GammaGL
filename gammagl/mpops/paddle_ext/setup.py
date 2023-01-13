8aa028bfcef3a33191f11b3fba73ea499c92a359
hanhui <193691140@qq.com>
2023-01-13 22:06:25 +0800

    use single thread on cpu

--- gammagl/mpops/paddle_ext/setup.py
+++ gammagl/mpops/paddle_ext/setup.py
@@ -2,10 +2,10 @@ import paddle
 from paddle.utils.cpp_extension import CppExtension, CUDAExtension, setup
 
 cuda_macro = ('COMPILE_WITH_CUDA', None) # Paddle offer `PADDLE_WITH_CUDA` macro
-omp_macro = ('COMPILE_WITH_OMP', None) # Note: OpenMP needs gcc>4.2.0
-compile_args = {
-    'cxx':['-fopenmp']
-}
+# omp_macro = ('COMPILE_WITH_OMP', None) # Note: OpenMP needs gcc>4.2.0
+# compile_args = {
+#     'cxx':['-fopenmp']
+# }
 
 def get_exts():
     if paddle.is_compiled_with_cuda():
@@ -16,8 +16,11 @@ def get_exts():
                 'cpu/segment_sum_cpu.cpp',
                 'cuda/segment_sum_cuda.cu',
             ],
-            define_macros=[omp_macro, cuda_macro],
-            extra_compile_args=compile_args
+            define_macros=[
+                cuda_macro, 
+                # omp_macro,
+                ],
+            # extra_compile_args=compile_args
         )
     else:
         return CppExtension(
@@ -25,8 +28,8 @@ def get_exts():
                 'segment_sum.cpp',
                 'cpu/segment_sum_cpu.cpp',
             ],
-            define_macros=[omp_macro],
-            extra_compile_args=compile_args
+            # define_macros=[omp_macro],
+            # extra_compile_args=compile_args
         )
 
 setup(
