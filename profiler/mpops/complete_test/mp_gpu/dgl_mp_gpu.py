99918209153cbe9db11b849f9172f13e40c2758f
BuptTab <gyzhou2000@gmail.com>
2023-03-01 11:11:50 +0800

    optimize code

--- profiler/mpops/complete_test/mp_gpu/dgl_mp_gpu.py
+++ profiler/mpops/complete_test/mp_gpu/dgl_mp_gpu.py
@@ -7,101 +7,47 @@ import time
 device = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
 # device = torch.device("cpu")
 
-# edge_index = np.load('/home/zgy/GammaGL/profiler/mpops/edge_index/cora.npy')
-# edge_index = np.load('/home/zgy/GammaGL/profiler/mpops/edge_index/pubmed.npy')
-edge_index = np.load('/home/zgy/GammaGL/profiler/mpops/edge_index/ogbn-arxiv.npy')
+relative_path = 'profiler/mpops/edge_index/'
+file_name = ['cora.npy', 'pubmed.npy', 'ogbn-arxiv.npy']
+embedding = [16, 64, 256]
+iter = 10
 
+for name in file_name:
+    path = relative_path + name
+    print(path)
+    edge_index = np.load(path)
 
-# edge_index = np.load('../../edge_index/cora.npy')
-# edge_index = np.load('../../edge_index/pubmed.npy')
-# edge_index = np.load('../../edge_index/ogbn-arxiv.npy')
-num_nodes = np.max(edge_index) + 1
-src = edge_index[0, :]
-dst = edge_index[1, :]
-# edge_index = edge_index.to(device)
-# num_nodes = num_nodes.to(device)
+    num_nodes = np.max(edge_index) + 1
+    src = edge_index[0, :]
+    dst = edge_index[1, :]
 
+    g = dgl.graph((edge_index[0], edge_index[1]))
+    g = g.to(device)
 
-g = dgl.graph((edge_index[0], edge_index[1]))
-g = g.to(device)
-# x = torch.tensor(np.random.randn(g.num_nodes(), 16))
-#
-# out = F.copy_u_sum(g, x)
-# print(out)
+    for embedding_dim in embedding:
+        print("**********embedding_dim={}**********".format(embedding_dim))
+        x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
+        x = x.to(device)
 
-print("**********embedding_dim=16**********")
-embedding_dim = 16
-x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
-x = x.to(device)
+        start = time.time()
+        for j in range(10):
+            F.copy_u_sum(g, x)
+        end = time.time()
+        print("copy_u_sum:{:.3f}".format(end - start))
 
-start = time.time()
-for j in range(10):
-    F.copy_u_sum(g, x)
-end = time.time()
-print("copy_u_sum:{:.3f}".format(end - start))
+        start = time.time()
+        for j in range(10):
+            F.copy_u_mean(g, x)
+        end = time.time()
+        print("copy_u_mean:{:.3f}".format(end - start))
 
-start = time.time()
-for j in range(10):
-    F.copy_u_mean(g, x)
-end = time.time()
-print("copy_u_mean:{:.3f}".format(end - start))
+        start = time.time()
+        for j in range(10):
+            F.copy_u_max(g, x)
+        end = time.time()
+        print("copy_u_max:{:.3f}".format(end - start))
 
-start = time.time()
-for j in range(10):
-    F.copy_u_max(g, x)
-end = time.time()
-print("copy_u_max:{:.3f}".format(end - start))
+        print("**********embedding_dim={}**********".format(embedding_dim))
 
-print("**********embedding_dim=16**********")
-
-print("**********embedding_dim=64**********")
-embedding_dim = 64
-x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
-x = x.to(device)
-
-start = time.time()
-for j in range(10):
-    F.copy_u_sum(g, x)
-end = time.time()
-print("copy_u_sum:{:.3f}".format(end - start))
-
-start = time.time()
-for j in range(10):
-    F.copy_u_mean(g, x)
-end = time.time()
-print("copy_u_mean:{:.3f}".format(end - start))
-
-start = time.time()
-for j in range(10):
-    F.copy_u_max(g, x)
-end = time.time()
-print("copy_u_max:{:.3f}".format(end - start))
-
-print("**********embedding_dim=64**********")
-
-print("**********embedding_dim=256**********")
-embedding_dim = 256
-x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
-x = x.to(device)
-
-start = time.time()
-for j in range(10):
-    F.copy_u_sum(g, x)
-end = time.time()
-print("copy_u_sum:{:.3f}".format(end - start))
-
-start = time.time()
-for j in range(10):
-    F.copy_u_mean(g, x)
-end = time.time()
-print("copy_u_mean:{:.3f}".format(end - start))
-
-start = time.time()
-for j in range(10):
-    F.copy_u_max(g, x)
-end = time.time()
-print("copy_u_max:{:.3f}".format(end - start))
-
-print("**********embedding_dim=256**********")
-print(x.device)
+    print(x.device)
 
--- profiler/mpops/complete_test/ops_gpu/pyg_scatter_ops_gpu.py
+++ profiler/mpops/complete_test/ops_gpu/pyg_scatter_ops_gpu.py
@@ -6,103 +6,51 @@ from torch_scatter import scatter_sum, scatter_mean, scatter_max
 device = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
 # device = torch.device("cpu")
 
-# edge_index = np.load('/home/zgy/GammaGL/profiler/mpops/edge_index/cora.npy')
-# edge_index = np.load('/home/zgy/GammaGL/profiler/mpops/edge_index/pubmed.npy')
-edge_index = np.load('/home/zgy/GammaGL/profiler/mpops/edge_index/ogbn-arxiv.npy')
+relative_path = 'profiler/mpops/edge_index/'
+file_name = ['cora.npy', 'pubmed.npy', 'ogbn-arxiv.npy']
+embedding = [16, 64, 256]
+iter = 10
 
-# edge_index = np.load('../../edge_index/cora.npy')
-# edge_index = np.load('../../edge_index/pubmed.npy')
-# edge_index = np.load('../../edge_index/ogbn-arxiv.npy')
-num_nodes = np.max(edge_index)+1
-src = edge_index[0,:]
-dst = edge_index[1,:]
-src = torch.tensor(src)
-dst = torch.tensor(dst)
-x = torch.tensor(np.random.randn(num_nodes, 1000))
-x = x.to(device)
-src = src.to(device)
-dst = dst.to(device)
+for name in file_name:
+    path = relative_path + name
+    print(path)
+    edge_index = np.load(path)
 
+    num_nodes = np.max(edge_index)+1
+    src = edge_index[0,:]
+    dst = edge_index[1,:]
+    src = torch.tensor(src)
+    dst = torch.tensor(dst)
+    x = torch.tensor(np.random.randn(num_nodes, 1000))
+    x = x.to(device)
+    src = src.to(device)
+    dst = dst.to(device)
 
-print("**********embedding_dim=16**********")
-embedding_dim = 16
-x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
-x = x.to(device)
+    for embedding_dim in embedding:
+        print("**********embedding_dim={}**********".format(embedding_dim))
+        x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
+        x = x.to(device)
 
-msg = x[src]
+        msg = x[src]
 
-start = time.time()
-for j in range(10):
-    scatter_sum(msg, dst, dim=0)
-end = time.time()
-print("scatter_sum:{:.3f}".format(end-start))
+        start = time.time()
+        for j in range(10):
+            scatter_sum(msg, dst, dim=0)
+        end = time.time()
+        print("scatter_sum:{:.3f}".format(end-start))
 
-start = time.time()
-for j in range(10):
-    scatter_mean(msg, dst, dim=0)
-end = time.time()
-print("scatter_mean:{:.3f}".format(end-start))
+        start = time.time()
+        for j in range(10):
+            scatter_mean(msg, dst, dim=0)
+        end = time.time()
+        print("scatter_mean:{:.3f}".format(end-start))
 
-start = time.time()
-for j in range(10):
-    scatter_max(msg, dst, dim=0)
-end = time.time()
-print("scatter_max:{:.3f}".format(end-start))
+        start = time.time()
+        for j in range(10):
+            scatter_max(msg, dst, dim=0)
+        end = time.time()
+        print("scatter_max:{:.3f}".format(end-start))
 
-print("**********embedding_dim=16**********")
+        print("**********embedding_dim={}**********".format(embedding_dim))
 
-
-print("**********embedding_dim=64**********")
-embedding_dim = 64
-x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
-x = x.to(device)
-
-msg = x[src]
-
-start = time.time()
-for j in range(10):
-    scatter_sum(msg, dst, dim=0)
-end = time.time()
-print("scatter_sum:{:.3f}".format(end-start))
-
-start = time.time()
-for j in range(10):
-    scatter_mean(msg, dst, dim=0)
-end = time.time()
-print("scatter_mean:{:.3f}".format(end-start))
-
-start = time.time()
-for j in range(10):
-    scatter_max(msg, dst, dim=0)
-end = time.time()
-print("scatter_max:{:.3f}".format(end-start))
-print("**********embedding_dim=64**********")
-
-
-print("**********embedding_dim=256**********")
-embedding_dim = 256
-x = torch.tensor(np.random.randn(num_nodes, embedding_dim))
-x = x.to(device)
-
-msg = x[src]
-
-start = time.time()
-for j in range(10):
-    scatter_sum(msg, dst, dim=0)
-end = time.time()
-print("scatter_sum:{:.3f}".format(end-start))
-
-start = time.time()
-for j in range(10):
-    scatter_mean(msg, dst, dim=0)
-end = time.time()
-print("scatter_mean:{:.3f}".format(end-start))
-
-start = time.time()
-for j in range(10):
-    scatter_max(msg, dst, dim=0)
-end = time.time()
-print("scatter_max:{:.3f}".format(end-start))
-print("**********embedding_dim=256**********")
-
-print(x.device)
+    print(x.device)
