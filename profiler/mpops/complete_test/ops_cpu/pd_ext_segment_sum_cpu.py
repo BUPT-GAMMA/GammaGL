e8f98918778d9b8ac18744e214ef084c78d0e8c3
Guangyu Zhou <77875480+gyzhou2000@users.noreply.github.com>
2023-03-02 13:50:47 +0800

    [Model] change han (#126)
    * test performance
    
    * rgcn model supports imdb dataset
    
    * retest effect
    
    * fix bug
    
    * optimize code
    
    ---------
    
    Co-authored-by: BuptTab <gyzhou2000@gmail.com>

---
+++ profiler/mpops/complete_test/ops_cpu/pd_ext_segment_sum_cpu.py
@@ -0,0 +1,51 @@
+import os
+
+os.environ['TL_BACKEND'] = 'paddle'
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+from pyinstrument import Profiler
+import numpy as np
+import tensorlayerx as tlx
+# from gammagl.mpops import *
+import time
+
+try:
+    import paddle_ext
+except ImportError:
+    print("not found paddle_segment")
+    exit(0)
+# import paddle_segment
+
+relative_path = 'profiler/mpops/edge_index/'
+file_name = ['cora.npy', 'pubmed.npy', 'ogbn-arxiv.npy']
+embedding = [16, 64, 256]
+iter = 10
+
+for name in file_name:
+    path = relative_path + name
+    print(path)
+    edge_index = np.load(path)
+
+
+    num_nodes = np.max(edge_index) + 1
+    src = edge_index[0, :]
+    dst = edge_index[1, :]
+    src = tlx.convert_to_tensor(src, tlx.int64)
+    dst = tlx.convert_to_tensor(dst, tlx.int64)
+    # x = tlx.convert_to_tensor(np.random.randn(num_nodes, embedding_dim), dtype=tlx.float32)
+    edge_index = tlx.convert_to_tensor(edge_index)
+
+    for embedding_dim in embedding:
+        print("**********embedding_dim={}**********".format(embedding_dim))
+
+        x = tlx.convert_to_tensor(np.random.randn(num_nodes, embedding_dim), dtype=tlx.float32)
+        msg = tlx.gather(x, src)
+
+        start = time.time()
+        for j in range(10):
+            paddle_ext.unsorted_segment_sum(msg, dst, num_nodes)
+        end = time.time()
+        print("ext_segment_max:{:.3f}".format(end-start))
+
+        print("**********embedding_dim={}**********".format(embedding_dim))
+
+    print(x.place)
