77811321e850e3f3a754fe2dcfdb72687146e4cc
wsy000718 <131266014+wsy000718@users.noreply.github.com>
2023-04-20 11:38:19 +0800

    Add files via upload

--- gammagl/models/gan.py
+++ gammagl/models/gan.py
@@ -1,36 +1,48 @@
 import tensorlayerx as tlx
 from gammagl.layers.conv import GANConv
+from .deepgcn import DeepGCNLayer
 
 class GANModel(tlx.nn.Module):
     def __init__(self,
                  feature_dim,
                  hidden_dim,
+                 edge_dim,
                  num_class,
                  drop_rate,
                  agg='sum',
                  num_layers=2,
                  name=None):
         super().__init__(name=name)
-        self.node_encoder = tlx.layers.Linear(out_features=hidden_dim, in_features=feature_dim)
-        self.layers = tlx.nn.ModuleList()
-        self.norm = tlx.nn.BatchNorm1d()
-        self.act = tlx.nn.ReLU()
-        self.drop = tlx.nn.Dropout(drop_rate)
+        self.drop_rate = drop_rate
         self.num_layers = num_layers
-        for i in range(num_layers):
-            self.layers.append(GANConv(in_channels=hidden_dim, out_channels=hidden_dim, agg=agg))
+        self.node_encoder = tlx.layers.Linear(out_features=hidden_dim, in_features=feature_dim,
+                                              W_init='xavier_uniform')
+        self.edge_encoder = tlx.layers.Linear(out_features=hidden_dim, in_features=edge_dim,
+                                              W_init='xavier_uniform')
 
-        self.lin = tlx.layers.Linear(out_features=num_class, in_features=hidden_dim)
+        self.layers = tlx.nn.ModuleList()
+        for i in range(1, num_layers + 1):
+            conv = GANConv(in_channels=hidden_dim, out_channels=hidden_dim, agg=agg)
+            norm = tlx.nn.LayerNorm(hidden_dim)
+            act = tlx.nn.ReLU()
+            layer = DeepGCNLayer(conv, norm, act, dropout=drop_rate)
+            self.layers.append(layer)
+        self.drop = tlx.nn.Dropout(drop_rate)
+        self.lin = tlx.layers.Linear(out_features=num_class, in_features=hidden_dim,
+                                     W_init='xavier_uniform')
 
-
-    def forward(self,x ,edge_index, edge_weight, num_nodes):
-
+    def forward(self, x, edge_index, edge_weight, num_nodes):
         x = self.node_encoder(x)
-        for i in range(self.num_layers):
-            x = self.norm(x)
-            x = self.act(x)
-            x = self.drop(x)
-            x = self.layers[i](x, edge_index, edge_weight, num_nodes)
+        edge_weight = self.edge_encoder(edge_weight)
+
+        x = self.layers[0].conv(x, edge_index, edge_weight, num_nodes)
+
+        for layer in self.layers[1:]:
+            x = layer(x, edge_index, edge_weight, num_nodes)
+
+        x = self.layers[0].act(self.layers[0].norm(x))
+        x = self.drop(x)
         return self.lin(x)
+
 
 
