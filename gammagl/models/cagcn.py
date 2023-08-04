97c7b6cd244eb2565520ccd3d48c2331937176a8
BuptTab <gyzhou2000@gmail.com>
2023-08-04 19:56:20 +0800

    Revert "delete useless code"
    This reverts commit b71cd23935ee643a77114b83f8d209b608a97246.

--- gammagl/models/cagcn.py
+++ gammagl/models/cagcn.py
@@ -1,4 +1,8 @@
+# import os
+# os.environ['TL_BACKEND'] = 'torch'
+
 import tensorlayerx as tlx
+import tensorlayerx.nn as nn
 from gammagl.models.gcn import GCNModel
 
 
@@ -32,20 +36,41 @@ class CAGCNModel(tlx.nn.Module):
         super().__init__()
         self.base_model = base_model
         self.cal_model = GCNModel(feature_dim, hidden_dim, num_class, drop_rate, num_layers, norm, name)
+        
+        # if necessary, reload trainable_weights by pre_trainable_weights
+        # self.pre_trainable_weights = self.trainable_weights
+        # for w in self.trainable_weights:
+        #     self.nontrainable_weights.append(w)
+        # self.trainable_weights = None
 
 
+    """
+    Parameters
+    ----------
+        cal_edge_index:     cal_model's edge index
+        cal_edge_weight:    cal_model's edge weight, if None set all weight 1.0
+                            default weight setting in gcn_conv
+        cal_num_nodes:      cal_model's num nodes
+        args && kwargs:     all paras required for base_model
+    """
     def forward(self, cal_edge_index, cal_edge_weight, cal_num_nodes, *args, **kwargs):
-        """
-        Parameters
-        ----------
-            cal_edge_index:     cal_model's edge index
-            cal_edge_weight:    cal_model's edge weight, if None set all weight 1.0
-                                default weight setting in gcn_conv
-            cal_num_nodes:      cal_model's num nodes
-            args && kwargs:     all paras required for base_model
-        """
+        # print("extra1:",args)
+        # print("extra2:", kwargs)
         logits = self.base_model(*args, **kwargs)
         t = self.cal_model(logits, cal_edge_index, cal_edge_weight, cal_num_nodes)
         t = tlx.nn.Softplus()(t)
         output = logits * t
         return output
+
+
+# if __name__ == '__main__':
+    # net = tlx.nn.Module()
+    # x = tlx.nn.Parameter(data=tlx.ones((5,5)), requires_grad=False)
+    # net.trainable_weights.append(x)
+    # print(net.trainable_weights)
+
+    # net = tlx.nn.Input([8, 100, 1], name='input')
+    # conv1d = tlx.nn.Conv1d(out_channels =32, kernel_size=5, stride=2, b_init=None, in_channels=1, name='conv1d_1')
+    # print(conv1d)
+    # tensor = tlx.nn.Conv1d(out_channels =32, kernel_size=5, stride=2, act=tlx.ReLU, name='conv1d_2')(net)
+    # print(tensor)
