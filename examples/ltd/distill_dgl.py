445ffeccc4832fcb8eb68ad52fcfa956d4c5d019
levi000 <1461678194@qq.com>
2023-11-02 20:28:57 +0800

    数据集cora，模型GCN正常

--- examples/ltd/distill_dgl.py
+++ examples/ltd/distill_dgl.py
@@ -9,6 +9,7 @@ sys.path.insert(0, os.path.abspath('../../'))  # adds path2gammagl to execute in
 from gammagl.utils import add_self_loops, remove_self_loops
 from gammagl.mpops import *
 from gammagl.models import GCNModel, GATModel, GraphSAGE_Sample_Model, APPNPModel, MLP, SGCModel
+import torch.optim as optim
 
 
 def accuracy(output, labels):
@@ -19,22 +20,33 @@ def accuracy(output, labels):
     return result / len(labels)
 
 
+def accuracy_t(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    label = labels.max(1)[1]
+    correct = preds.eq(label).double()
+    result = correct.sum()
+
+    return result / len(labels)
+
+
 def choose_model(configs, dataset):
     if configs['model_name'] == 'GCN':
         model = GCNModel(feature_dim=dataset.num_node_features,
-                         hidden_dim=configs['hidden'],
+                         hidden_dim=64,
                          num_class=dataset.num_classes,
-                         drop_rate=configs['dropout'],
-                         num_layers=1,
+                         drop_rate=0.8,
+                         num_layers=2,
                          norm='both',
                          name='GCN')
     elif configs['model_name'] == 'GAT':
         model = GATModel(feature_dim=dataset.num_node_features,
-                         hidden_dim=8,
-                         num_class=dataset.num_classes,
-                         heads=8,
-                         drop_rate=0.6,
-                         num_layers=1)
+                       hidden_dim=8,
+                       num_class=dataset.num_classes,
+                       heads=8,
+                       drop_rate=0.4,
+                       num_layers=3,
+                       name="GAT",
+                       )
     elif configs['model_name'] == 'GraphSAGE':
         model = GraphSAGE_Sample_Model(in_feat=dataset.num_node_features,
                                        hid_feat=16,
@@ -62,87 +74,6 @@ def choose_model(configs, dataset):
     return model
 
 
-def distill_loss(configs, labels_one_hot, student_hard, teacher_softmax, train_mask):
-    hard_loss = -tlx.ops.reduce_sum(
-        (labels_one_hot[train_mask] + 1e-6) * tlx.ops.log(student_hard[train_mask] + 1e-6))
-    soft_loss = -tlx.ops.reduce_sum((teacher_softmax + 1e-6) * tlx.ops.log(student_hard + 1e-6))
-    loss = soft_loss + configs['lam'] * hard_loss
-    return loss
-
-
-def evaluate_loss(logits, y, num_classes, val_mask):
-    student_hard = tlx.ops.softmax(logits, axis=1)
-    student_logits = student_hard[val_mask]
-    labels_one_hot = tlx.nn.OneHot(depth=num_classes)(y)[val_mask]
-    loss = -tlx.ops.reduce_sum(
-        (labels_one_hot + 1e-6) * tlx.ops.log(student_logits + 1e-6))
-    return loss
-
-
-class SemiSpvzLoss(WithLoss):
-    def __init__(self, net, loss_fn, graph, configs, nei_entropy, t_model, teacher_logits, num_classes, train_mask):
-        super(SemiSpvzLoss, self).__init__(backbone=net, loss_fn=loss_fn)
-        self.configs = configs
-        self.graph = graph
-        self.nei_entropy = nei_entropy
-        self.t_model = t_model
-        self.teacher_logits = teacher_logits
-        self.num_classes = num_classes
-        self.train_mask = train_mask
-
-    def forward(self, data, y):
-        edge_index, _ = add_self_loops(self.graph.edge_index, num_nodes=self.graph.num_nodes)
-        if self.configs['model_name'] in ['GCN', 'LogReg', 'MLP']:
-            logits = self.backbone_network(data, edge_index, None, self.graph.num_nodes)
-        if self.configs['model_name'] in ['APPNP']:
-            logits = self.backbone_network(data)
-        elif self.configs['model_name'] == 'GAT':
-            logits, _, fs = self.backbone_network(data)
-        elif self.configs['model_name'] in ['GraphSAGE', 'SGC']:
-            logits = self.backbone_network(data)
-        with torch.no_grad():
-            f_logits = torch.norm(logits, 2, dim=1)  # L2 范数
-        extract_x = tlx.ops.concat(
-            [logits, tlx.expand_dims(f_logits, axis=1), tlx.expand_dims(self.nei_entropy, axis=1)],
-            axis=1)
-
-        temparature = self.t_model(extract_x)
-        temparature = (temparature - 0.2) * self.configs['k']
-        temparature = tlx.ops.where(tlx.ops.abs(temparature) < 0.0001, 0.001 * tlx.ops.ones_like(temparature),
-                                    temparature)
-
-        teacher_logits_t = tlx.ops.transpose(
-            tlx.ops.divide(tlx.ops.transpose(self.teacher_logits), tlx.ops.squeeze(temparature, axis=1)))
-        teacher_softmax = tlx.ops.softmax(teacher_logits_t, axis=1)
-        student_hard = tlx.ops.softmax(logits, axis=1)
-        labels_one_hot = tlx.nn.OneHot(depth=self.num_classes)(y)
-        loss = self._loss_fn(self.configs, labels_one_hot, student_hard, teacher_softmax, self.train_mask)
-        return loss
-
-
-class t_SemiSpvzLoss(WithLoss):
-    def __init__(self, net, loss_fn, graph, configs, num_classes, model, val_mask):
-        super(t_SemiSpvzLoss, self).__init__(backbone=net, loss_fn=loss_fn)
-        self.graph = graph
-        self.configs = configs
-        self.num_classes = num_classes
-        self.model = model
-        self.val_mask = val_mask
-
-    def forward(self, data, y):
-        edge_index, _ = add_self_loops(self.graph.edge_index, num_nodes=self.graph.num_nodes)
-        if self.configs['model_name'] in ['GCN', 'LogReg', 'MLP']:
-            logits = self.model(data, edge_index, None, self.graph.num_nodes)
-        if self.configs['model_name'] in ['APPNP']:
-            logits = self.model(data)
-        elif self.configs['model_name'] == 'GAT':
-            logits, _, fs = self.model(data)
-        elif self.configs['model_name'] in ['GraphSAGE', 'SGC']:
-            logits = self.model(data)
-        loss = self._loss_fn(logits, y, self.num_classes, self.val_mask)
-        return loss
-
-
 def zero_grad(my_model):  # grad->0
     with torch.no_grad():
         for p in my_model.parameters():
@@ -150,55 +81,117 @@ def zero_grad(my_model):  # grad->0
                 p.grad.zero_()
 
 
-def distill_train(dur, epoch, model, configs, graph, dataset, temp, nei_entropy, t_model, teacher_logits, train_mask,
-                  val_mask, my_val_mask):
+def distill_train(epoch, model, configs, graph, nei_entropy, t_model, teacher_logits, train_mask,
+                  val_mask, my_val_mask, dataset):
     model.set_train()
     edge_index, _ = add_self_loops(graph.edge_index, num_nodes=graph.num_nodes)
     if configs['model_name'] in ['GCN', 'LogReg', 'MLP']:
         logits = model(graph.x, edge_index, None, graph.num_nodes)
+    elif configs['model_name'] == 'GAT':
+        logits = model(graph.x, edge_index, graph.num_nodes)
+    acc_train = accuracy(logits[train_mask], graph.y[train_mask])
+    acc_same = accuracy_t(logits, teacher_logits)
+    # acc_teacher = accuracy(teacher_logits, graph.y)
     with torch.no_grad():
         f_logits = torch.norm(logits, 2, dim=1)  # L2 范数
     extract_x = tlx.ops.concat(
         [logits, tlx.expand_dims(f_logits, axis=1), tlx.expand_dims(nei_entropy, axis=1)],
         axis=1)
 
+    # optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # su = tlx.reduce_sum(nei_entropy)
     temparature = t_model(extract_x)
-    temparature = (temparature - 0.2) * configs['k']
+    t_max = torch.max(temparature)
+    t_min = torch.min(temparature)
+    temparature = (temparature + 0.2) * configs['k']
     temparature = tlx.ops.where(tlx.ops.abs(temparature) < 0.0001, 0.001 * tlx.ops.ones_like(temparature),
                                 temparature)
-
+    t_sum = tlx.reduce_sum(temparature)
+    # t = tlx.ops.squeeze(temparature, axis=1)
     teacher_logits_t = tlx.ops.transpose(
         tlx.ops.divide(tlx.ops.transpose(teacher_logits), tlx.ops.squeeze(temparature, axis=1)))
     teacher_softmax = tlx.ops.softmax(teacher_logits_t, axis=1)
+    # teacher_softmax = tlx.ops.softmax(teacher_logits, axis=1)
     student_hard = tlx.ops.softmax(logits, axis=1)
     labels_one_hot = tlx.nn.OneHot(depth=dataset.num_classes)(graph.y)
     hard_loss = -tlx.ops.reduce_sum(
         (labels_one_hot[train_mask] + 1e-6) * tlx.ops.log(student_hard[train_mask] + 1e-6))
+    # hard_loss = tlx.losses.softmax_cross_entropy_with_logits(logits[train_mask], graph.y[train_mask])
     soft_loss = -tlx.ops.reduce_sum((teacher_softmax + 1e-6) * tlx.ops.log(student_hard + 1e-6))
+    # for p_name, p in model.named_parameters():
+    #     for name, t in t_model.named_parameters():
+    #         agr = torch.autograd.grad(tlx.reduce_sum(t), p, create_graph=True)[0]
+    #         print(agr)
     train_loss = soft_loss + configs['lam'] * hard_loss
+    # train_loss = 1 * soft_loss + 0 * hard_loss
     model_dict = {}
+
+    grad_temp = {}
     for p_name, p in model.named_parameters():
         agr = torch.autograd.grad(train_loss, p, create_graph=True)[0]
         model_dict[p_name] = p - configs['my_lr'] * agr  # update model 【公式10】
-    model.load_state_dict(model_dict)
+        # for name, t in t_model.named_parameters():
+        #     if t.requires_grad:
+        #         g = torch.autograd.grad(tlx.reduce_sum(model_dict[p_name]), t, create_graph=True)[0]
+        #         grad_temp[p_name] = {}
+        #         grad_temp[p_name][name] = g
+        # if epoch >= 20:
+        #     for name, t in t_model.named_parameters():
+        #         if t.requires_grad:
+        #             g = torch.autograd.grad(tlx.reduce_sum(model_dict[p_name]), t, create_graph=True)[0]
+        #             grad_temp[p_name] = {}
+        #             grad_temp[p_name][name] = g
+
+    model.load_state_dict(model_dict, strict=False)
+    # zero_grad(model)
+
+    # optimizer.zero_grad()
+    # train_loss.backward()
+    # optimizer.step()
 
     t_train_loss = torch.tensor(0)
-    t_model_dict = {}
     if epoch > 20:
         edge_index, _ = add_self_loops(graph.edge_index, num_nodes=graph.num_nodes)
         if configs['model_name'] in ['GCN', 'LogReg', 'MLP']:
             logits = model(graph.x, edge_index, None, graph.num_nodes)
+        elif configs['model_name'] == 'GAT':
+            logits = model(graph.x, edge_index, graph.num_nodes)
         student_hard = tlx.ops.softmax(logits, axis=1)
         student_logits = student_hard[val_mask]
         labels_one_hot = tlx.nn.OneHot(depth=dataset.num_classes)(graph.y)[val_mask]
         t_train_loss = -tlx.ops.reduce_sum(
             (labels_one_hot + 1e-6) * tlx.ops.log(student_logits + 1e-6))
+        # t_train_loss = tlx.losses.softmax_cross_entropy_with_logits(logits[val_mask], graph.y[val_mask])
 
-        for pvt_name, pt in t_model.named_parameters():  # t1,t2
-            pt.grad = torch.autograd.grad(t_train_loss, pt, create_graph=True)[0]  # 求model中的参数对tmodel中参数的梯度
-            t_model_dict[pvt_name] = pt - configs['my_t_lr'] * pt.grad
+        # for name, t in model.named_parameters():
+        #     print(name)
+        #     print(t)
+        # for pvt_name, pt in t_model.named_parameters():  # t1,t2
+        #     if pt.requires_grad:
+        #         pt.grad = torch.autograd.grad(train_loss, pt, create_graph=True)[0]  # 求model中的参数对tmodel中参数的梯度
+        #         t_model_dict[pvt_name] = pt - configs['my_t_lr'] * pt.grad
+        for name, t in model.named_parameters():
+            t_grad = torch.autograd.grad(t_train_loss, t, create_graph=True, allow_unused=True)[0]
+            t_grad = t_grad.detach()
+            for p_name, p in model.named_parameters():
+                if t_grad.shape == p.shape:
+                    model_dict[p_name] = model_dict[p_name] * t_grad
+        t_model_dict = {}
+        for p_name, p in model.named_parameters():
+            for name, t in t_model.named_parameters():
+                # agr = torch.autograd.grad(model_dict[p_name], t, create_graph=True, allow_unused=True)[0]
+                agr = torch.autograd.grad(tlx.reduce_sum(model_dict[p_name]), t, create_graph=True, allow_unused=True)[
+                    0]
+                t_model_dict[name] = t - configs['my_lr'] * agr
 
-        t_model.load_state_dict(t_model_dict)
+        t_model.load_state_dict(t_model_dict, strict=False)
+        # for p_name, p_t in t_model.named_parameters():
+        #     # if p_t.requires_grad and grad_temp[name][p_name]:
+        #     # pt_grad = torch.autograd.grad(tlx.reduce_sum(model_dict[name]), p_t, create_graph=True)[0]
+        #     grad = t_grad * grad_temp[name][p_name]
+        #     t_model_dict[p_name] = p_t - configs['my_t_lr'] * grad
+
+        # t_model.load_state_dict(t_model_dict)
         zero_grad(t_model)
     model.set_eval()
     edge_index, _ = add_self_loops(graph.edge_index, num_nodes=graph.num_nodes)
@@ -207,14 +200,16 @@ def distill_train(dur, epoch, model, configs, graph, dataset, temp, nei_entropy,
     if configs['model_name'] in ['APPNP']:
         logits = model(graph.x)
     elif configs['model_name'] == 'GAT':
-        logits, _, fs = model(graph.x)
+        logits = model(graph.x, edge_index, graph.num_nodes)
     elif configs['model_name'] in ['GraphSAGE', 'SGC']:
         logits = model(graph.x)
-    logp = tlx.ops.log(tlx.ops.softmax(logits))
+    logp = tlx.ops.softmax(logits)
     acc_val = accuracy(logp[my_val_mask], graph.y[my_val_mask])
     loss_val = 0
-    print('Epoch %d | acc_val: %.4f | distill_loss: %.4f | evaluate_loss: %.4f' % (
-    epoch, acc_val.item(), train_loss.item(), t_train_loss.item()))
+    print(
+        'Epoch %d | acc_train: %.4f| acc_val: %.4f | acc_same: %.4f | hard_loss: %.4f | soft_loss: %.4f| distill_loss: %.4f | evaluate_loss: %.4f' % (
+            epoch, acc_train.item(), acc_val.item(), acc_same.item(), hard_loss.item(), soft_loss.item(), train_loss.item(),
+            t_train_loss.item()))
     return acc_val, loss_val
 
 
@@ -237,34 +232,63 @@ def split_train_mask(graph, num_classes):
 
 
 def model_train(configs, model, graph, teacher_logits, dataset):
-    all_logits = []
-    dur = []
-    best = 0
     cnt = 0
     epoch = 1
-
     teacher_softmax = tlx.ops.softmax(teacher_logits, axis=1)
+    # print(teacher_logits[0],teacher_softmax[0])
+    # #
+    # teacher_softmax = torch.tensor([[0.7, 0.3], [0.6, 0.4], [0.8, 0.2], [0.1, 0.9]])
+    # t = tlx.reduce_sum(teacher_softmax[0])
     edge_index_no_self_loops, _ = remove_self_loops(graph.edge_index)
+    ################################
+    # edge_index_no_self_loops = torch.tensor([[0,0,1,1,1,2,2,2,3,3],[1,2,0,2,3,0,1,3,1,2]])
     msg = tlx.gather(teacher_softmax, edge_index_no_self_loops[1])
     nei_logits_sum = unsorted_segment_sum(msg, edge_index_no_self_loops[0], graph.num_nodes)
-    nei_num = unsorted_segment_sum(tlx.ops.ones_like(graph.edge_index[1, :]), edge_index_no_self_loops[0],
+    # nei_logits_sum = unsorted_segment_sum(msg, edge_index_no_self_loops[0], 4)
+    # # PyTorch 示例
+    # data = torch.tensor([[1, 2, 3, 4, 5, 6],[12, 2, 3, 4, 5, 6],[15, 2, 3, 4, 5, 6]])
+    # segment_ids = torch.tensor([1, 0, 1, 1, 2, 2])
+    # msg = tlx.gather(data, segment_ids)
+    # num_segments = 3  # 分段的数量
+    #
+    # # 使用 unsorted_segment_sum 对数据进行分段求和
+    # result = unsorted_segment_sum(data, segment_ids, num_segments)
+    nei_num = unsorted_segment_sum(tlx.ops.ones_like(edge_index_no_self_loops[0]), edge_index_no_self_loops[0],
                                    graph.num_nodes)  # 分段统计
+    # nei_num = unsorted_segment_sum(tlx.ops.ones_like(edge_index_no_self_loops[0]), edge_index_no_self_loops[0],
+    #                                                               4)  # 分段统计
+    # 总的边数
+    # print(tlx.reduce_sum(nei_num))
     nei_probability = tlx.ops.transpose(tlx.ops.divide(tlx.ops.transpose(nei_logits_sum), nei_num))  # 邻居汇总/邻居个数
+    # su = tlx.reduce_sum(nei_probability[0])
     nei_entropy = -tlx.ops.reduce_sum(nei_probability * tlx.ops.log(nei_probability), axis=1)
     for i in range(len(nei_entropy)):
         if nei_entropy[i] != nei_entropy[i]:
             nei_entropy[i] = 0.0001
 
-    t_model = MLP(num_layers=2, in_channels=dataset.num_classes + 2, hidden_channels=64, out_channels=1, dropout=0.6)
-
+    t_model = MLP(num_layers=2, in_channels=dataset.num_classes + 2, hidden_channels=64, out_channels=1, dropout=0.4,
+                  norm=None)
+    # print(t_model)
     temp = 0
     best = 0
     train_mask, val_mask, my_val_mask = split_train_mask(graph, dataset.num_classes)
-    print("train_val_my %d %d %d" % (
-    torch.sum(train_mask).item(), torch.sum(val_mask).item(), torch.sum(my_val_mask).item()))
+    # # 统计每个掩码中1的数量
+    # train_mask_count = torch.sum(train_mask)
+    # val_mask_count = torch.sum(val_mask)
+    # my_val_mask_count = torch.sum(my_val_mask)
+    # test_mask_count = torch.sum(graph.test_mask)
+    #
+    # # 统计每个掩码对应的标签的数量
+    # train_label_count = torch.bincount(graph.y[train_mask.bool()])
+    # val_label_count = torch.bincount(graph.y[val_mask.bool()])
+    # my_val_label_count = torch.bincount(graph.y[my_val_mask.bool()])
+    # test_val_label_count = torch.bincount(graph.y[graph.test_mask.bool()])
+    #
+    # print(train_label_count, val_label_count, my_val_label_count, test_val_label_count, train_mask_count,
+    #       val_mask_count, my_val_mask_count, test_mask_count)
     while epoch < configs['max_epoch']:
-        acc_val, loss_val = distill_train(dur, epoch, model, configs, graph, dataset, temp, nei_entropy, t_model,
-                                          teacher_logits, train_mask, val_mask, my_val_mask)
+        acc_val, loss_val = distill_train(epoch, model, configs, graph, nei_entropy, t_model,
+                                          teacher_logits, train_mask, val_mask, my_val_mask, dataset)
         epoch += 1
         if epoch > 0:
             if acc_val >= best:
--- examples/ltd/hypersearch.py
+++ examples/ltd/hypersearch.py
@@ -3,6 +3,7 @@ import psutil
 from utils import choose_path, load_cascades
 from distill_dgl import choose_model, model_train
 import sys
+import torch
 
 sys.path.insert(0, os.path.abspath('../../'))  # adds path2gammagl to execute in command line.
 from gammagl.datasets import Planetoid, Amazon
@@ -25,5 +26,13 @@ def raw_experiment(configs):
         raise ValueError('Unknown dataset: {}'.format(configs['dataset']))
     # 获得教师模型的预测结果
     teacher_logits = load_cascades(cascade_dir)
+    # 查看教师模型预测结果
+    # print(teacher_logits.shape)
+    # print(torch.sum(teacher_logits[0]))
     model = choose_model(configs, dataset)
+    print(model)
+    # for p_name, p in model.named_parameters():
+    #     p.data = torch.zeros(p.shape)
+    # 查看学生模型
+    # print(model)
     acc_val = model_train(configs, model, graph, teacher_logits, dataset)
