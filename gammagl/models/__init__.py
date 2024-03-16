45527de5e7f5fe6f2ef2f152652751f11a76a2e8
followall <1438701787@qq.com>
2024-03-16 22:41:55 +0800

    add FusedGAT model

--- gammagl/models/__init__.py
+++ gammagl/models/__init__.py
@@ -52,6 +52,7 @@ from .grace_pot import Grace_POT_Encoder, Grace_POT_Model
 from .sfgcn import SFGCNModel
 from .grace_spco import Grace_Spco_Encoder, Grace_Spco_Model
 from .graphormer import Graphormer
+from .fusedgat import FusedGATModel
 
 __all__ = [
     'GCNModel',
@@ -107,7 +108,9 @@ __all__ = [
     'GGDModel',
     'Specformer',
     'SFGCNModel',
-    'Graphormer'
+    'Graphormer',
+    'FusedGATModel',
+    
 ]
 
 classes = __all__
