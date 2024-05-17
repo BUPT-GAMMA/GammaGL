911eeee10d1ab593996de776ccebb9584c361aaf
tanjjjjrr <133502760+tanjjjjrr@users.noreply.github.com>
2024-05-17 21:49:22 +0800

    Update HeCo_encoder.py

--- gammagl/layers/attention/HeCo_encoder.py
+++ gammagl/layers/attention/HeCo_encoder.py
@@ -6,6 +6,30 @@ from layers.attention.meta_path_attention import Attention
 from layers.attention.network_schema_attention import intra_att
 from layers.attention.network_schema_attention import inter_att
 import os
+
+import numpy as np
+import tensorlayerx as tlx
+import tensorlayerx.nn as nn
+
+class metapathSpecificGCN(nn.Module):
+    def __init__(self, in_ft, out_ft, bias=True):
+        super(metapathSpecificGCN, self).__init__()
+        self.fc = nn.Linear(in_features=in_ft, out_features=out_ft, W_init="he_normal")
+        self.act = nn.LeakyReLU()
+
+        if bias:
+            initor = tlx.initializers.Zeros()
+            self.bias = self._get_weights("bias", shape=(1, out_ft), init=initor)
+        else:
+            self.register_parameter('bias', None)
+
+
+    def forward(self, seq, adj):
+        seq_fts = self.fc(seq)
+        out = tlx.matmul(adj, seq_fts) 
+        if self.bias is not None:
+            out += self.bias
+        return self.act(out)
 class Sc_encoder(nn.Module):
     def __init__(self, hidden_dim, sample_rate, nei_num, attn_drop):
         super(Sc_encoder, self).__init__()
