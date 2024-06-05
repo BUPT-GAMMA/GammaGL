84b53e8eb5e6904f1542c1fc7bbbc7b606687f91
jxy <865526875@qq.com>
2024-06-05 07:00:21 +0000

    update __init__.py

--- gammagl/data/__init__.py
+++ gammagl/data/__init__.py
@@ -6,6 +6,8 @@ from .download import download_url
 from .in_memory_dataset import InMemoryDataset
 from .extract import extract_zip, extract_tar
 from .utils import global_config_init
+from .feature_store import FeatureStore, TensorAttr
+from .graph_store import GraphStore, EdgeAttr
 
 __all__ = [
     'BaseGraph',
@@ -18,6 +20,10 @@ __all__ = [
     'extract_zip',
     'extract_tar',
     'global_config_init',
+    'FeatureStore',
+    'TensorAttr',
+    'GraphStore',
+    'EdgeAttr',
 ]
 
 classes = __all__
