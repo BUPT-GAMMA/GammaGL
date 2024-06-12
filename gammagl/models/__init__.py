1da343db024acc1763d6068524d6888e22b52a45
tanjjjjrr <133502760+tanjjjjrr@users.noreply.github.com>
2024-06-13 00:01:31 +0800

    Update __init__.py

--- gammagl/models/__init__.py
+++ gammagl/models/__init__.py
@@ -52,7 +52,7 @@ from .grace_pot import Grace_POT_Encoder, Grace_POT_Model
 from .sfgcn import SFGCNModel
 from .grace_spco import Grace_Spco_Encoder, Grace_Spco_Model
 from .graphormer import Graphormer
-from .HeCo import HeCo
+from .heco import HeCo
 __all__ = [
     'HeCo',
     'GCNModel',
