e870ed075646e68f4cc4ba6b5252eda8ea178edb
tanjjjjrr <133502760+tanjjjjrr@users.noreply.github.com>
2024-06-13 00:01:07 +0800

    Update __init__.py

--- gammagl/layers/attention/__init__.py
+++ gammagl/layers/attention/__init__.py
@@ -2,8 +2,8 @@ from .centrality_encoder import CentralityEncoding
 from .edge_encoder import EdgeEncoding
 from .spatial_encoder import SpatialEncoding
 from .graphormer_layer import GraphormerLayer
-from .HeCo_encoder import Sc_encoder
-from .HeCo_encoder import Mp_encoder
+from .heco_encoder import Sc_encoder
+from .heco_encoder import Mp_encoder
 __all__ = [
     'Sc_encoder',
     'Mp_encoder',
