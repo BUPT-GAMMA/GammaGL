6ddba3ad7630633167284ce6131b20994b659929
gyzhou2000 <gyzhou@bupt.edu.cn>
2024-06-25 09:39:19 +0000

    add svd feature reduction

--- gammagl/transforms/__init__.py
+++ gammagl/transforms/__init__.py
@@ -6,6 +6,7 @@ from .normalize_features import NormalizeFeatures
 from .drop_edge import DropEdge
 from .random_link_split import RandomLinkSplit
 from .vgae_pre import mask_test_edges, sparse_to_tuple
+from .svd_feature_reduction import SVDFeatureReduction
 
 __all__ = [
     'BaseTransform',
@@ -16,7 +17,8 @@ __all__ = [
     'DropEdge',
     'RandomLinkSplit',
     'mask_test_edges',
-    'sparse_to_tuple'
+    'sparse_to_tuple',
+    'SVDFeatureReduction'
 
 ]
 
